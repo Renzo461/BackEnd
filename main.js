68369bcdfdec704060e96ef0123eadcba09e1b29
Renzo Rosales <72679166@continental.edu.pe>
2022-07-27 23:35:32 -0500

    Plantillas

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 const express = require('express')
 const productos = require('./modulos/productos')
 const app = express()
+
+// // ejs
+// app.set('view engine', 'ejs')
+
+// //pug
+// app.set('view engine', 'pug')
+// app.set('views', './views/pug')
+
+// hbs
+const handlebars = require('express-handlebars')
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs',
+  defaultlayout: 'main.hbs',
+  // eslint-disable-next-line n/no-path-concat
+  layoutsDir: __dirname + '/views/hbs/layouts',
+  // eslint-disable-next-line n/no-path-concat
+  partialsDir: __dirname + '/views/hbs/partials'
+}))
+app.set('view engine', 'hbs')
+app.set('views', './views/hbs')
+
+// SERVER
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use('/api/productos', productos)
 app.listen(8080, () => {
-    console.log('Servidor OK puerto 8080')
+  console.log('Servidor OK puerto 8080')
 })
--- modulos/productos.js
+++ modulos/productos.js
@@ -1,82 +1,87 @@
+/* eslint-disable eqeqeq */
 const express = require('express')
 const { Router } = express
 const router = Router()
 
 const productos = [
-    {
-        "title": "Escuadra",
-        "price": 123.45,
-        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-        "id": 1
-    },
-    {
-        "title": "Calculadora",
-        "price": 234.56,
-        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
-        "id": 2
-    },
-    {
-        "title": "Globo Terráqueo",
-        "price": 345.67,
-        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
-        "id": 3
-    }
+  {
+    title: 'Escuadra',
+    price: 123.45,
+    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+    id: 1
+  },
+  {
+    title: 'Calculadora',
+    price: 234.56,
+    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',
+    id: 2
+  },
+  {
+    title: 'Globo Terráqueo',
+    price: 345.67,
+    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',
+    id: 3
+  }
 ]
 
 router.get('/', (req, res) => {
-    res.send(productos)
+  // res.render('articulo', { productos }) //pug
+  res.render('articulo', { prod: productos }) // hbs
+  // res.render('ejs/layouts/main', { prod: productos }) // ejs
 })
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params
-    if (isNaN(id)) {
-        res.status(400).send({ error: 'El parámetro no es un número' });
-        return
-    }
-    const p = productos.find((e) => e.id == id)
-    if (p == undefined) {
-        res.status(404).send({ error: 'producto no encontrado' });
-        return
-    }
-    res.send(p)
+  const { id } = req.params
+  if (isNaN(id)) {
+    res.status(400).send({ error: 'El parámetro no es un número' })
+    return
+  }
+  const p = productos.find((e) => e.id == id)
+  if (p === undefined) {
+    res.status(404).send({ error: 'producto no encontrado' })
+    return
+  }
+  res.send(p)
 })
 router.post('/', (req, res) => {
-    const productoAGuardar = req.body;    
-    productos.push(productoAGuardar);    
-    res.status(201).send({ statusPost: 'ok' })    
+  const productoAGuardar = {
+    ...req.body,
+    id: productos.length + 1
+  }
+  productos.push(productoAGuardar)
+  res.redirect('../index.html')
 })
 
 router.put('/:id', (req, res) => {
-    const productoAActualizar = req.body;
-    const { id } = req.params
-    if (isNaN(id)) {
-        res.status(400).send({ error: 'El parámetro no es un número' });
-        return
-    }
-    const p = productos.find((e) => e.id == id)
-    if (p == undefined) {
-        res.status(404).send({ error: 'producto no encontrado' });
-        return
-    }
-    const index = productos.indexOf(p)
-    productos[index] = productoAActualizar
-    res.send({ statusPut: 'ok' })
+  const productoAActualizar = req.body
+  const { id } = req.params
+  if (isNaN(id)) {
+    res.status(400).send({ error: 'El parámetro no es un número' })
+    return
+  }
+  const p = productos.find((e) => e.id == id)
+  if (p == undefined) {
+    res.status(404).send({ error: 'producto no encontrado' })
+    return
+  }
+  const index = productos.indexOf(p)
+  productos[index] = productoAActualizar
+  res.send({ statusPut: 'ok' })
 })
 router.delete('/:id', (req, res) => {
-    const { id } = req.params
-    if (isNaN(id)) {
-        res.status(400).send({ error: 'El parámetro no es un número' });
-        return
-    }
-    const p = productos.find((e) => e.id == id)
-    if (p == undefined) {
-        res.status(404).send({ error: 'producto no encontrado' });
-        return
-    }
-    const index = productos.indexOf(p)
-    productos.splice(index, 1)
-    res.send({ statusDelete: 'ok' })
+  const { id } = req.params
+  if (isNaN(id)) {
+    res.status(400).send({ error: 'El parámetro no es un número' })
+    return
+  }
+  const p = productos.find((e) => e.id == id)
+  if (p == undefined) {
+    res.status(404).send({ error: 'producto no encontrado' })
+    return
+  }
+  const index = productos.indexOf(p)
+  productos.splice(index, 1)
+  res.send({ statusDelete: 'ok' })
 })
-
 
 module.exports = router
